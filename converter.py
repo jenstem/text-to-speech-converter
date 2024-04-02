d688b749d3320ed659900244674375983cfc1090
Jen <jennifer.stemkowski@gmail.com>
2024-04-02 18:18:35 -0400

    Convert user input into speech + add  Tkinter window

--- converter.py
+++ converter.py
@@ -1,13 +1,30 @@
 from gtts import gTTS
 import os
+from tkinter import *
 
 
-text = open('converter.txt', 'r').read()
+root = Tk()
 
 
-language = 'en'
+canvas = Canvas(root, width=400, height=300)
+canvas.pack()
 
 
-output = gTTS(text=text, lang=language, slow=False)
-output.save('fileoutput.mp3')
-os.system("afplay fileoutput.mp3")
+def convert():
+    text = entry.get()
+    language = 'en'
+    output = gTTS(text=text, lang=language, slow=False)
+    output.save("output.mp3")
+    os.system("afplay output.mp3")
+
+
+entry = Entry(root)
+canvas.create_window(200, 180, window=entry)
+canvas.create_text(200, 140, text='Enter text to convert to speech')
+
+
+button = Button(text='Start', command=lambda: convert())
+canvas.create_window(200, 230, window=button)
+
+
+root.mainloop()
